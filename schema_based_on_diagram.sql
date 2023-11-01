4496f940ac8eefdfd2a380ff9235887cc975e539
Arch-Noize <santiagonoize@gmail.com>
2023-11-01 06:05:14 -0400

    Add Foreign Keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -33,3 +33,32 @@ CREATE TABLE treatments (
     type VARCHAR(100),
     name VARCHAR(100)
 );
+
+CREATE TABLE med_history_treatment (
+    med_history_id INT,
+    treatment_id INT    
+);
+
+ALTER TABLE medical_histories
+ADD CONSTRAINT patient_fk FOREIGN KEY(patient_id)
+REFERENCES patients(id);
+
+ALTER TABLE invoices
+ADD CONSTRAINT medical_histories_id_fk FOREIGN KEY(medical_histories_id)
+REFERENCES medical_histories(id);
+
+ALTER TABLE invoice_items
+ADD CONSTRAINT invoice_fk FOREIGN KEY(invoice_id)
+REFERENCES invoices(id);
+
+ALTER TABLE invoice_items
+ADD CONSTRAINT treatment_fk FOREIGN KEY(treatment_id)
+REFERENCES treatments(id);
+
+ALTER TABLE med_history_treatment
+ADD CONSTRAINT med_history_id_fk FOREIGN KEY(med_history_id)
+REFERENCES medical_histories(id);
+
+ALTER TABLE med_history_treatment
+ADD CONSTRAINT treatment_id_fk FOREIGN KEY(treatment_id)
+REFERENCES treatments(id);
